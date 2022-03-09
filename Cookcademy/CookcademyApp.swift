f1b0f675462d5e5fbecc94ccb03070b2c3207ea8
Joao Mauricio <jfmauricio5@gmail.com>
2022-03-09 15:11:33 +0000

    Initial Commit

---
+++ Cookcademy/CookcademyApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookcademyApp.swift
+//  Cookcademy
+//
+//  Created by João Mauricio UWE ID 18030724 on 09/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookcademyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
